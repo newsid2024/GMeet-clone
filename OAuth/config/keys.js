c62f28f7ce00b2078755876ed12c28ad2c808823
Sid2024 <85190865+Sid2024@users.noreply.github.com>
2022-07-12 02:33:45 +0530

    Initial commit

---
+++ OAuth/config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '23148247538-ibg9v4e3j483m4o6bapm2cubkc2lr4i1.apps.googleusercontent.com',
+            clientSecret: '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'
+        },
+        mongodb: {
+            dbURI: 'mongodb+srv://devansh1356:sharan@cluster0.yeba5.mongodb.net/test'
+        },
+        session: {
+            cookieKey: 'thenetninjaisawesomeiguess'
+        }
+};
